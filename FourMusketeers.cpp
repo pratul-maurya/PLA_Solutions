e426451871458abf03c352fd72156b6f799fd6a0
Saransh Pratap Singh <74720434+SARANSH694@users.noreply.github.com>
2021-09-17 14:25:27 +0530

    FourMuskeeteers upload

---
+++ FourMusketeers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a1,b1,a2,b2,a3,b3;
+
+    cin>>a1>>b1>>a2>>b2>>a3>>b3;
+
+    float a4,b4;
+    a4=(a1+a2+a3)/3;
+    b4=(b1+b2+b3)/3;
+
+    cout<<a4<<b4;
+    
+    return 0;
+}
