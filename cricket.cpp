8d78815d67063972de46fa41bb26d29e4fba1fa9
PRATUL MAURYA <70075276+pratul-m5799@users.noreply.github.com>
2021-09-17 14:49:49 +0530

    Create cricket.cpp

---
+++ cricket.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+    int t_balls, cballs, truns, cruns;
+    float r_runrate, c_runrate, t_overs, c_overs;
+    cin>>t_balls>>truns>>cruns>>cballs;
+    
+    int c_o;
+    float remanining_balls;
+    c_o=cballs/6;
+    remanining_balls=cballs%6;
+    remanining_balls=remanining_balls/10;
+
+    c_overs=c_o + remanining_balls;
+    t_overs=t_balls/6;
+    c_runrate=cruns/c_overs;
+    r_runrate=truns/t_overs;
+    
+    cout<<endl<<t_overs<<" "<<c_overs<<" "<<c_runrate<<" "<<r_runrate;
+}
