9bfef49088a122042631c7fa7a70dafd1b7f34fc
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2021-09-24 07:59:19 +0530

    Add files via upload

---
+++ The Newspaper Agency.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    int p=(a*b)-(a*c)-100;
+    
+    cout<<p;
+
+    return 0;
+}
