e10d862a9f8de20ebe7c6d70486a7c2a41a1c7c0
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-09-27 13:44:50 +0530

    Create prime_numbers_till_n.cpp

---
+++ prime_numbers_till_n.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool checkPrime(int n) //function to check whether a number is prime
+{
+    if (n<=1)           //corner case
+        return false;
+
+    for ( int i=2;i < n;i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i=2; i<=n; i++)
+    {
+        if(checkPrime(i))
+            cout<<i<<" ";
+    }
+    return 0;
+}
