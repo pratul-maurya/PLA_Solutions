c999ca0f089689d2b845cac54fc550074ed6958d
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-09-29 14:13:11 +0530

    Update angle_between_hour_and_minute_hand.cpp

--- angle_between_hour_and_minute_hand.cpp
+++ angle_between_hour_and_minute_hand.cpp
@@ -15,3 +15,27 @@ int main()
 
     return 0;
 }
+
+/*
+Angle between hour and minute hand
+Let us consider the normal wall clock we use. Write a program to calculate the smallest angle between the hour 'H' and the minute 'M' hand of a clock, for the given time.
+Set the precision to two decimal points. Print "Invalid Input" if the time is invalid.
+
+Constraints:
+0 < H <= 12
+0 <= M < 60
+
+Example:
+Input:
+3 30
+Output:
+75.00
+	 Case 1
+	 Case 2
+
+Input (stdin)
+3 30
+Output (stdout)
+75.00
+
+*/
