26b71524e91acd27dafbfb942381bb771dc54d56
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-09-30 10:49:30 +0530

    Update azad_and_choclates.cpp

--- azad_and_choclates.cpp
+++ azad_and_choclates.cpp
@@ -10,14 +10,14 @@ int main()
         cin>>a[i];
     }
 
-    for (int j = 0; j < n; j++)
+    for (int j = 0; j < n; j++) //to check whether he bought the choclate from the shop or not
     {
-        rem=100-a[j];
+        rem=100-a[j]; //remaining amount
         rem7=rem % 7;
 
-        if((rem % 3 == 0) || (rem % 7 == 0))
+        if((rem % 3 == 0) || (rem % 7 == 0)) //if rem amount is div by 3 or 7 he then bought it from shop
             cout<<1<<endl;
-        else if(rem7 % 3 == 0)
+        else if(rem7 % 3 == 0) //if rem amount is div by 3 AND 7 he also then bought it from shop
             cout<<1<<endl;
         else
             cout<<0<<endl;
