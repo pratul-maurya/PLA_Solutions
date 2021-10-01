df57a078b3e1c23a1d9d93880db0f7a4ba445bd4
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-10-01 14:08:01 +0530

    Create numer_of_odd_factors_in_a_range.cpp

---
+++ numer_of_odd_factors_in_a_range.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int oddSquares(int a, int b) //squares have odd factors
+{
+    a=sqrt(a-1);
+    b=sqrt(b);
+    return (b-a);
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<oddSquares(a,b);
+    return 0;
+}
