01204bf80bcf3ee106ef56050551ae3311cb375b
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-10-04 14:01:12 +0530

    Create shortest_number_sol_1.cpp

---
+++ shortest_number_sol_1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int reverse(int n) 
+//reverse a number to 
+{
+    int rev=0;
+    while(n!=0)
+    {
+        rev=(rev*10)+(n % 10);
+        n/=10;
+    }
+    return rev;
+}
+
+int main()
+{
+    int n,n1=0;
+    int k;
+    cin>>n>>k;
+    n=reverse(n);
+    for (int i = 0; i < k; i++)
+    {
+        n=n/10;
+    }
+    n=reverse(n);
+    cout<<n;
+}
