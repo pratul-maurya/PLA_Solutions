ba5ee9a14a9aecc61a7fa70c15bada238d696ae7
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-10-18 14:07:46 +0530

    Create betting_game.cpp

---
+++ betting_game.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    int final_amount(string s)
+    {
+        int amt=10;
+        int bet =1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if(s[i]=='W')
+                {
+                    amt+=bet;
+                    if(bet != 1)
+                    {
+                        bet/=2;
+                    }
+                    else 
+                        bet=1;
+                }
+            else
+            {
+                amt-=bet;
+                bet*=2;
+            }
+        }
+        
+            return amt;
+    }
+};
+
+int main()
+{
+    Solution res;
+    string s;
+    cin>>s;
+    cout<<res.final_amount(s);
+}
