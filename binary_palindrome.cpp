d9941d696c396aa18cdb124cb43d1d44194415e5
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-10-20 00:34:15 +0530

    Update binary_palindrome.cpp

--- binary_palindrome.cpp
+++ binary_palindrome.cpp
@@ -4,27 +4,30 @@ using namespace std;
 class Solution
 {
     public:
-     bool isBitSet(unsigned int x, unsigned int k)
-     {
-         return ( x& (1 << (k-1))) ? true : fasle;
-     }
-
      bool isPalindrome(unsigned int x)
      {
-         int l = 1;
-         int r = sizeof(unsigned int) * 8;
-
-         while (l < r)
+         unsigned rev = 0;
+         unsigned k = x;
+         
+         while (k)
          {
-            if(isBitSet(x, l) != isBitSet(x, r))
-                return false;
+             rev = (rev << 1) | (k & 1);
+             k >>= 1;
          }
          
+         return (x==rev);         
      }
 
 };
 
 int main()
 {
-
+    Solution res;
+    int x;
+    cin>>x;
+    
+    if(res.isPalindrome(x))
+        cout<<"yes";
+    else
+        cout<<"no";
 }
