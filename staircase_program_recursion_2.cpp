4c5ec800f92abd24138433224190dace7966d17f
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-10-28 23:24:22 +0530

    Update staircase_program_recursion_2.cpp

--- staircase_program_recursion_2.cpp
+++ staircase_program_recursion_2.cpp
@@ -2,3 +2,25 @@
 using namespace std;
 
 class Solution
+{
+    public:
+    int steps(int n)
+    {
+        int res[n + 1];
+        res[0] = 1;
+        res[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            res[i]=res[i - 1] + res[i - 2];
+        }
+        return res[n];
+    }
+};
+
+int main()
+{
+    Solution res;
+    int n;
+    n=4;
+    cout<<res.steps(n);
+}
